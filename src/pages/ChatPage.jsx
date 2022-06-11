635f84f867776b7bbad426d919b2121749d78587
gospargs <gospargs@gmail.com>
2022-06-12 00:08:13 +0200

    Not ready for members yet.

--- src/pages/ChatPage.jsx
+++ src/pages/ChatPage.jsx
@@ -9,14 +9,12 @@ export function ChatPage() {
   
   const { user, drone, logOut } = useContext(UserContext)
   const [messages, setMessages] = useState([])
-  const [members, setMembers] = useState({ online: [] });
   const [userIsLoggingOut, setUserIsLoggingOut] = useState(false)
   const navigate = useNavigate()
 
   useEffect(() => {
     if (user) {
       const room = drone.subscribe('observable-room')
-
       room.on('message', (message) => {
         setMessages((current) => {
           return [
@@ -32,23 +30,10 @@ export function ChatPage() {
           ]
         })
       })
-
-      room.on("members", (member) => {
-        members.online = member;
-        setMembers({...members})
-      });
-      room.on("member_join", (member) => {
-        members.online.push(member);
-        setMembers({ ...members });
-      });
-      room.on("member_leave", ({ id }) => {
-        const index = members.online.findIndex((member) => member.id === id);
-        members.online.splice(index, 1);
-        setMembers({ ...members });
-      });
     }
-  }, [user, drone,members])
+  }, [user, drone])
 
+ 
   useEffect(() => {
     if (userIsLoggingOut) {
       logOut()
@@ -64,7 +49,7 @@ export function ChatPage() {
 
   return (
     <div className='chat-app'>
-      <Header username={user.username} setUserIsLoggingOut={setUserIsLoggingOut} member = {members}></Header>
+      <Header username={user.username} setUserIsLoggingOut={setUserIsLoggingOut}></Header>
       <Messages messages={messages} messageFromMe={user.id}></Messages>
       <Input onSendMessage={onSendMessage} ></Input>
     </div>
