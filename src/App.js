2317262d62976b435c066e5f68d8ee3e9fe86748
gospargs <gospargs@gmail.com>
2022-06-25 16:30:28 +0200

    On refresh back to main page.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
-import React from "react";
 import { Route,Routes } from "react-router-dom";
 import { ChatPage } from "./pages/ChatPage";
 import { UserPage } from "./pages/UserPage";
-
+import { UserContext } from '../src/contexts/UserContext'
+import React, { useContext } from 'react'
 export default function App() {
+
+  const userState = useContext(UserContext)
+
   return (
     <Routes>
-      <Route index element={<UserPage/>}/> 
+      <Route index element={ !userState.user ? <UserPage/> : <ChatPage/>}/> 
       <Route path="chat" element={<ChatPage/>}/> 
     </Routes>
 
